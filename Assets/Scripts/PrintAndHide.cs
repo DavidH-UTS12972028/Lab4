75cea313d85f9c19f996e1c4d6b31246f5cccc12
DavidH-UTS12972028 <12972028@student.uts.edu.au>
2020-08-17 09:57:25 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
 {
     public Renderer rend;
+
     private int i = 0;
+    private int rand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rand = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(gameObject.name + ": " + i++);
+
+        if (gameObject.tag == "Red" && i == 100)
+            gameObject.SetActive(false);
+
+        if (gameObject.tag == "Blue" && i == 100)
+            rend.enabled = false;
     }
 }
